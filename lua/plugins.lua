361bf3dc9ab667895e5f9e2761e392f29edbe4b5
wdhg <me@wdhg.me>
2023-06-26 10:21:19 +0100

    Add packer bootstrapping

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local status, packer = pcall(require, 'packer')
 
 if (not status) then
@@ -31,4 +44,8 @@ packer.startup(function(use)
   use 'wuelnerdotexe/vim-astro'                    -- astro
   -- Rust
   use 'simrat39/rust-tools.nvim'                   -- rust
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
