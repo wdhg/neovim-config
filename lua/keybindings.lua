edc8f2114514e509dbcb194858f505692fce579a
wdhg <me@wdhg.me>
2024-04-02 20:21:01 +0100

    Merge F1 keybinds

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,6 +1,5 @@
 local keymap = vim.keymap
 
 keymap.set('n', 'r', ':redo<cr>')
-keymap.set('n', '<f1>', '<nop>')
-keymap.set('i', '<f1>', '<nop>')
+keymap.set({'n', 'i'}, '<f1>', '<nop>')
 keymap.set('n', '<tab>', ':Telescope file_browser<cr>', { noremap = true, silent = true })
