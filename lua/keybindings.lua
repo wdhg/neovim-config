45094b7fe852e5e0a308fade391f9a51fcacf036
wdhg <me@wdhg.me>
2024-04-08 20:30:14 +0100

    Improve run keybinds

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,3 +3,26 @@ local keymap = vim.keymap
 keymap.set('n', 'r', ':redo<cr>')
 keymap.set({'n', 'i'}, '<f1>', '<nop>')
 keymap.set('n', '<tab>', ':Telescope file_browser<cr>', { noremap = true, silent = true })
+
+local run_code = function(type)
+	return function()
+		if type == 'repeat' then
+				vim.fn.system('tmux send-keys -t 2 Up Enter')
+				return
+		end
+
+		local ft = vim.bo.filetype
+		if ft == 'rust' then
+			if type == 'run' then
+				vim.fn.system('tmux send-keys -t 2 "cargo run" Enter')
+			elseif type == 'test' then
+				vim.fn.system('tmux send-keys -t 2 "cargo test" Enter')
+			end
+		end
+	end
+end
+
+keymap.set('n', '<leader>rr', run_code('run'))
+keymap.set('n', '<leader>rt', run_code('test'))
+keymap.set('n', '<leader>rb', run_code('build'))
+keymap.set('n', '<leader>rx', run_code('repeat'))
