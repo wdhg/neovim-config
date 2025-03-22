b6356ab8040b0b5862ab58183b876f7080fde127
wdhg <me@wdhg.me>
2025-03-22 12:57:32 +0000

    Align on spaces

--- lua/base.lua
+++ lua/base.lua
@@ -2,25 +2,25 @@ vim.g.mapleader = ' '
 
 -- display
 vim.opt.termguicolors = true
-vim.scriptencoding = 'UTF-8'
-vim.opt.encoding = 'UTF-8'
-vim.opt.fileencoding = 'UTF-8'
+vim.scriptencoding    = 'UTF-8'
+vim.opt.encoding      = 'UTF-8'
+vim.opt.fileencoding  = 'UTF-8'
 
 -- gui
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.list = true
-vim.opt.colorcolumn = '100'
-vim.opt.inccommand = 'nosplit'
+vim.opt.number       = true
+vim.opt.cursorline   = true
+vim.opt.list         = true
+vim.opt.colorcolumn  = '100'
+vim.opt.inccommand   = 'nosplit'
 vim.opt.conceallevel = 0
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn   = 'yes'
 vim.diagnostic.config {
-	virtual_text = true,
-	signs = true,
+	virtual_text     = true,
+	signs            = true,
 	update_in_insert = true,
-	underline = true,
-	severity_sort = true,
-	float = {
+	underline        = true,
+	severity_sort    = true,
+	float            = {
 			border = 'rounded',
 			source = 'always',
 			header = '',
@@ -29,21 +29,21 @@ vim.diagnostic.config {
 }
 
 -- indenting
-vim.opt.cindent = true
-vim.opt.expandtab = false
-vim.opt.smarttab = true
-vim.opt.tabstop = 8
+vim.opt.cindent     = true
+vim.opt.expandtab   = false
+vim.opt.smarttab    = true
+vim.opt.tabstop     = 8
 vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 8
-vim.opt.list = true
-vim.opt.listchars = 'tab:▏ ,trail:█'
+vim.opt.shiftwidth  = 8
+vim.opt.list        = true
+vim.opt.listchars   = 'tab:▏ ,trail:█'
 vim.opt.breakindent = true
-vim.opt.linebreak = true
+vim.opt.linebreak   = true
 
 -- functionality
-vim.opt.mouse = 'a'
-vim.opt.exrc = true
-vim.opt.secure = true
+vim.opt.mouse      = 'a'
+vim.opt.exrc       = true
+vim.opt.secure     = true
 vim.opt.foldmethod = 'marker'
 
 -- formatoptions
@@ -54,9 +54,9 @@ vim.cmd('au BufRead,BufNewFile *.h set filetype=c')
 
 -- providers
 -- TODO check python is available via asdf before setting these
-vim.g.python_host_prog = '~/.asdf/installs/python/2.7.18/bin/python'
+vim.g.python_host_prog  = '~/.asdf/installs/python/2.7.18/bin/python'
 vim.g.python3_host_prog = '~/.asdf/installs/python/3.12.2/bin/python'
-vim.g.node_host_prog = '~/.asdf/installs/nodejs/21.6.2/bin/neovim-node-host'
+vim.g.node_host_prog    = '~/.asdf/installs/nodejs/21.6.2/bin/neovim-node-host'
 
 -- updatetime
 vim.api.nvim_set_option('updatetime', 300)
