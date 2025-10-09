2bdc04075df534148c415ee361d1672deeb77097
wdhg <me@wdhg.me>
2025-10-09 21:04:05 +0100

    Add loading project local configs

--- init.lua
+++ init.lua
@@ -4,3 +4,18 @@ require('base')
 require('plugins')
 require('syntax')
 require('keybindings')
+
+function load_local_config()
+	package.loaded['local']  = nil -- Unload module if it is already loade
+	local cwd                = vim.fn.getcwd()
+	local config_path        = cwd .. '/.nvim'
+	local code = os.execute('[ -d ' .. config_path .. ' ]')
+	if code ~= 0 then
+		return
+	end
+	package.path = package.path .. ';' .. config_path .. '/?.lua'
+	require('local') -- .nvim/local.lua
+	print(config_path .. ' loaded')
+end
+
+load_local_config()
